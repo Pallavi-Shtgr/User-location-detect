c202f36ce516d49381cc27754c0ce0b7d24fbaa5
Pallavi <pallavishettigar03@gmail.com>
2024-01-23 21:40:37 +0530

    add changes via upload

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// const button = document.querySelector("button");
+const button = document.querySelector("button");
 
-// button.addEventListener("click", ()=> {
-//     if (navigator.geolocation){
-//         navigator.geolocation.getCurrentPosition(onSuccess, onerror)
+button.addEventListener("click", ()=> {
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess, onerror)
 
-//     } else {
-//         button.innerText = " Your browser is not support";
-//     }
-// })
+    } else {
+        button.innerText = " Your browser is not support";
+    }
+})
 
-// function onSuccess(position){
-//     console.log(position)
-// }
+function onSuccess(position){
+    console.log(position)
+}
 
-// function onError(error) {
-//     if(error.code == 1) {
-//         button.innerText = "You denied the request";
-//     }
-//     else if (error.code == 2){
-//         button.innerText = "Location not available";
-//     } else {
-//         button.innerText = "Something went wrong";
-//     }
-// }
+function onError(error) {
+    if(error.code == 1) {
+        button.innerText = "You denied the request";
+    }
+    else if (error.code == 2){
+        button.innerText = "Location not available";
+    } else {
+        button.innerText = "Something went wrong";
+    }
+}
