d6aaaa0484983bc9917e5965f8b2a93f25f15d95
Pallavi <pallavishettigar03@gmail.com>
2024-01-28 11:12:22 +0530

    add changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", ()=> {
     if (navigator.geolocation){
+        button.innerText ="Allow to detect lcation";
         navigator.geolocation.getCurrentPosition(onSuccess, onerror)
 
     } else {
@@ -11,15 +12,20 @@ button.addEventListener("click", ()=> {
 
 function onSuccess(position){
     // console.log(position)
+    button.innerText=" detect your location";
     let{latitude, longitude} = position.coords;
      fetch('https://api.opencagedata.com/geocode/v1/json?q=$latitude}+${longitude}&key=${apiKey}')
      .then(response => response.json()).then(result => {
 
         let allDetails = result.results[0].components;
         let {country, postcode , country} = allDetails;
-        button.innerText='${country}';
+        button.innerText='${country} ${postcode},${country}';
+        console.table(allDetails);
 
-     })
+     }).catch((=> {
+        button.innerText = "Something went wrong";
+
+     }))
 }
 
 function onError(error) {
